51a5266cb9e49545ccbff0f1862954a1891207c4
谢云帆 <820013069@qq.com>
2020-07-23 20:55:40 +0800

    Unknown

--- Setup/Setup.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    URLDownloadToFile(NULL,¡°http://www.baidu.com/img/shouye_b5486898c692066bd2cbaeda86d74448.gif¡±,"C:\\Users\\harry\\Desktop\\Pictures\\pic1.jpg",0,NULL);
-    return 0;
-}
